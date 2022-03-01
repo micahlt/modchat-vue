be118b5872e8fc862ef862223c209b69628f93ad
Micah Lindley <micahincorporated@gmail.com>
2022-03-01 15:24:19 -0600

    📦 Remove vue-mouseover

--- src/main.js
+++ src/main.js
@@ -1,47 +1,43 @@
-import {
-  createApp
-} from 'vue';
-import VueSafeHTML from 'vue-safe-html';
-import Markdown from 'vue3-markdown-it';
-import Mouseover from 'vue-mouseover';
-import App from './App.vue';
+import { createApp } from "vue";
+import VueSafeHTML from "vue-safe-html";
+import Markdown from "vue3-markdown-it";
+import App from "./App.vue";
 import runtime from "serviceworker-webpack-plugin/lib/runtime";
 if ("serviceWorker" in navigator) {
   runtime.register();
 }
-switch (window.localStorage.getItem('theme')) {
-  case (null): {
-    window.localStorage.setItem('theme', 'default');
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+switch (window.localStorage.getItem("theme")) {
+  case null: {
+    window.localStorage.setItem("theme", "default");
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
       document.documentElement.classList.add("dark");
     } else {
       document.documentElement.classList.add("light");
     }
     break;
   }
-  case ('default'): {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  case "default": {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
       document.documentElement.classList.add("dark");
     } else {
       document.documentElement.classList.add("light");
     }
     break;
   }
-  case ('dark'): {
+  case "dark": {
     document.documentElement.classList.add("dark");
     break;
   }
-  case ('light'): {
+  case "light": {
     document.documentElement.classList.add("light");
     break;
   }
 }
 
 let app = createApp(App);
-app.use(Mouseover);
 app.use(Markdown);
 app.use(VueSafeHTML, {
-  allowedTags: ['a'],
+  allowedTags: ["a"],
 });
-app.mount('#app');
-import './theme/variables.css';
+app.mount("#app");
+import "./theme/variables.css";
